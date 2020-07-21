aea3f3c32eb628f97167042f22c0c2758e697bb4
andrew041994 <andrew041994@gmail.com>
2020-07-20 23:07:29 -0400

    added icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
-import { Button, Container, Row, Col } from 'react-bootstrap';
-import { Image } from 'react-bootstrap'
-
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles';
+import logo from '../profilePic.jpg'
+// import { Button, Container, Row, Col, Image } from 'react-bootstrap';
+// import Image from 'react-bootstrap/Image'
 export default class NavBar extends Component {
 
-//  handleClick = (e) => {
-//    e.preventDefault()
-  
-// }
+
 
 
   render() {
     return (
-      <div style={{"padding":"20px",
-      "colorTheme": "dark"}}>
+      <div style={{"padding":"20px"
+      }}>
+        <Avatar alt="A" src={logo} />
+         {/* <img src="/home/andrew/my-site/public/profilePic.jpg"/>
         <Container> 
           <Row> 
-          <Col xs={8} md={8}>
-            <Image src="public/profile pic.jpg" roundedCircle />
-          </Col>
+           
+          <Col xs={6} md={4}>
+      <Image src="holder.js/public/profilePic.jpg" roundedCircle />
+    </Col>
           <Col xs={1} md={1}>
             <Button onClick={() => window.open("https://github.com/andrew041994")} variant="primary"><strong>GitHub</strong></Button>
           </Col>  
@@ -27,7 +28,7 @@ export default class NavBar extends Component {
             <Button onClick={() => window.open("https://twitter.com/andrew041994")} variant="primary"><strong>Twitter</strong></Button>
            </Col>
             </Row>
-        </Container>
+        </Container> */}
        </div>
     );
   }
