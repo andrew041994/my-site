aa8c2c3d9784549e460b303a6a8a6f03aa89b66e
andrew041994 <andrew041994@gmail.com>
2020-07-22 01:18:26 -0400

    linkedin icon

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,6 +4,7 @@ import Avatar from '@material-ui/core/Avatar';
 import { makeStyles } from '@material-ui/core/styles';
 import twitter from '../images/twitter.png'
 import git from '../images/git.png'
+import linkedin from '../images/linkedin.png'
 // {{"width" : "40px", "height" : "40px", "cursor" : "pointer"}}
 const useStyles = makeStyles({
   gitStyle: {
@@ -13,7 +14,7 @@ const useStyles = makeStyles({
     cursor: 'pointer',
     position: 'relative',
     top: '-50px',
-    left: '1100px'
+    left: '1500px'
     },
 
   twitterStyle: {
@@ -23,8 +24,18 @@ const useStyles = makeStyles({
     cursor: 'pointer',
     position: 'relative',
     top: '-98px',
-    left: '1170px'
+    left: '1570px'
     },
+
+  linkedinStyle: {
+    width: '50px',
+    height: '50px',
+    backgroundColor: 'dark',
+    cursor: 'pointer',
+    position: 'relative',
+    top: '-144px',
+    left: '1640px'
+    }
 
 })
 
@@ -42,6 +53,11 @@ export default function Nav() {
           <div onClick={() => window.open("https://twitter.com/andrew041994")} >
             <Avatar className={classes.twitterStyle} alt="T" src={twitter} />
           </div>
+
+          <div onClick={() => window.open("https://www.linkedin.com/in/alehandro-persaud/")} >
+            <Avatar className={classes.linkedinStyle} alt="L" src={linkedin} />
+          </div>
+
         <Button onClick={() => window.open("https://github.com/andrew041994")} 
       variant="contained" color="primary"><strong>GitHub</strong></Button>
          
