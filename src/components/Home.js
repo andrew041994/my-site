ca4e115bd1d2747fe7ef80e9f5acaa0c9818a5f8
andrew041994 <andrew041994@gmail.com>
2020-07-24 00:14:20 -0400

    refactor code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import Logo from './Logo';
 import Nav from './nav';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-         {/* <Logo /> */}
+      
          <Nav />
          
          </div>
--- src/components/nav.js
+++ src/components/nav.js
@@ -6,7 +6,9 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
-// import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import TwitterIcon from '@material-ui/icons/Twitter';
 import logo from '../images/profilePic.jpg'
 // import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
@@ -25,6 +27,13 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  iconStyle: {
+    marginRight: theme.spacing(2),
+    cursor: 'pointer',
+    width: '30px',
+    height:'30px'
+  }
+
 }));
 
 export default function NavBar() {
@@ -34,13 +43,13 @@ export default function NavBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-        <div onClick={()=> window.location.reload(true)}>
-        <Avatar alt="A" src={logo} className={classes.logoStyle} />         
-       </div>
-          <Typography variant="h6" className={classes.title}>
-            Alehandro Persaud
-          </Typography>
-          <Button color="inherit">Login</Button>
+           <Avatar onClick={()=> window.location.reload(true)} alt="A" src={logo} className={classes.logoStyle} />           
+           <Typography variant="h6" className={classes.title}>
+              Alehandro Persaud
+           </Typography>
+           <GitHubIcon className={classes.iconStyle} onClick={() => window.open("https://github.com/andrew041994")}/>
+           <LinkedInIcon className={classes.iconStyle} onClick={() => window.open("https://www.linkedin.com/in/alehandro-persaud/")}/>
+           <TwitterIcon className={classes.iconStyle} onClick={() => window.open("https://twitter.com/andrew041994")}/>
         </Toolbar>
       </AppBar>
     </div>
