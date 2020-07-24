49a9f06dbf828cb1988749caac38d9fd8f7a3293
andrew041994 <andrew041994@gmail.com>
2020-07-24 00:39:16 -0400

    refactor code

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,15 +4,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import logo from '../images/profilePic.jpg'
-// import wallPaper from '../public/wallPaper.png'
-// import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -59,77 +56,4 @@ export default function NavBar() {
 }
 
 
-
-
-
-// import React from 'react';
-// import Button from '@material-ui/core/Button';
-// import Avatar from '@material-ui/core/Avatar';
-// import { makeStyles } from '@material-ui/core/styles';
-// import twitter from '../images/3dtwitter.jpg'
-// import git from '../images/3dgit.jpg'
-// import linkedin from '../images/3dlinkedin.png'
-// // {{"width" : "40px", "height" : "40px", "cursor" : "pointer"}}
-// const useStyles = makeStyles({
-//   gitStyle: {
-//     display: 'block',
-//     width: '50px',
-//     height: '50px',
-//     backgroundColor: 'dark',
-//     cursor: 'pointer',
-//     position: 'fixed',
-//     top: '50px',
-//     right: '400px'
-//     },
-
-//   twitterStyle: {
-//     display: 'block',
-//     width: '50px',
-//     height: '50px',
-//     backgroundColor: 'dark',
-//     cursor: 'pointer',
-//     position: 'fixed',
-//     top: '50px',
-//     right: '320px'
-//     },
-
-//   linkedinStyle: {
-//     display: 'block',
-//     width: '50px',
-//     height: '50px',
-//     backgroundColor: 'dark',
-//     cursor: 'pointer',
-//     position: 'fixed',
-//     top: '50px',
-//     right: '240px'
-//     }
-
-// })
-
-// export default function Nav() {
-  
-//   const classes = useStyles();
-  
-//     return (
-//       <div color="primary"> 
-//           <div  onClick={() => window.open("https://github.com/andrew041994")} >
-//           <Avatar className={classes.gitStyle} alt="G" src={git} />
-//           </div>
-//           <div onClick={() => window.open("https://twitter.com/andrew041994")} >
-//             <Avatar className={classes.twitterStyle} alt="T" src={twitter} />
-//           </div>
-
-//           <div onClick={() => window.open("https://www.linkedin.com/in/alehandro-persaud/")} >
-//             <Avatar className={classes.linkedinStyle} alt="L" src={linkedin} />
-//           </div>
-
-//         {/* <Button onClick={() => window.open("https://github.com/andrew041994")} 
-//       variant="contained" color="primary"><strong>GitHub</strong></Button>
-         
-//       <Button onClick={() => window.open("https://twitter.com/andrew041994")} 
-//       variant="contained" color="primary"><strong>Twitter</strong></Button> */}
-      
-//       </div>
-//     );
-//   }
 
